8e615a3d7583e03a340eac5d140b2f466d0753b6
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2024-09-02 20:56:19 +0530

    Time: 585 ms (31.93%) | Memory: 30.7 MB (19.92%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -3,10 +3,9 @@ class Solution:
         tc = sum(chalk)
         k %= tc
         c = 0
-        while k > 0:
-            for i in range(len(chalk)):
-                k -= chalk[i]
-                if k < 0:
-                    break
-                c += 1
+        for i in range(len(chalk)):
+            k -= chalk[i]
+            if k < 0:
+                break
+            c += 1
         return(c % len(chalk))
