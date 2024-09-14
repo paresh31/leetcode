2d991c7a4d031e8818819ccf18339c2e162c6c28
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2024-09-14 14:12:06 +0530

    Time: 34 ms (99.70%) | Memory: 16.6 MB (35.67%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
@@ -1,6 +1,8 @@
 class Solution:
     def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        l = len(nums1)
-        s1 = sum(nums1)
-        s2 = sum(nums2)
-        return((s2//l) - (s1//l))
+        # l = len(nums1)
+        # s1 = sum(nums1)
+        # s2 = sum(nums2)
+        # return((s2//l) - (s1//l))
+
+        return (sum(nums2)//len(nums1)) - sum(nums1)//len(nums1)
