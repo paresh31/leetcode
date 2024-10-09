a867a555e52874c062626b9045da72f28e2af9c5
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2024-10-09 20:51:03 +0530

    Time: 36 ms (48.13%) | Memory: 16.5 MB (30.64%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -1,21 +1,35 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        c = 0
-        stack = list()
-        for i in s:
-            if len(stack) == 0 and i == "(":
-                stack.append(i)
-                continue
-            if len(stack) == 0 and i == ")":
-                c += 1
-                continue
-            if i == "(":
-                stack.append(i)
-            elif i == ")" and stack[-1] == "(":
-                stack.pop()
+        op, cl = 0, 0
+        for item in s:
+            if item == "(":
+                op += 1
+            elif item == ")" and op > 0:
+                op -= 1
             else:
-                c += 1
-        return len(stack) + c
+                cl += 1
+        return op + cl
+
+
+
+
+
+        # c = 0
+        # stack = list()
+        # for i in s:
+        #     if len(stack) == 0 and i == "(":
+        #         stack.append(i)
+        #         continue
+        #     if len(stack) == 0 and i == ")":
+        #         c += 1
+        #         continue
+        #     if i == "(":
+        #         stack.append(i)
+        #     elif i == ")" and stack[-1] == "(":
+        #         stack.pop()
+        #     else:
+        #         c += 1
+        # return len(stack) + c
             
         
 
