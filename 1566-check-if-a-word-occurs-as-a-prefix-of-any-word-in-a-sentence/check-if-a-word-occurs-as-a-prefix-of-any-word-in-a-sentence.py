8bca2c1c0e31d4e9d7a824e0c9d7add244d88293
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2024-12-02 10:39:32 +0530

    Time: 1 ms (100.00%) | Memory: 17.5 MB (5.67%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPrefixOfWord(self, s: str, t: str) -> int:
+        s=' '+s
+        t=' '+t
+        n, m=len(s), s.find(t)
+        if m==-1: return -1
+        return 1+s[:m].count(' ')
