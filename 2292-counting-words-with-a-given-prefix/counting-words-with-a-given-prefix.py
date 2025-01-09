e53e7f8320c22607a66958de7136f009bb1e5de4
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-01-09 17:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (9.54%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -3,14 +3,13 @@ class Solution:
         pl = len(pref)
         res = 0
         for word in words:
+            count = 0
             if len(word) < pl:
                 continue
             else:
-                count = 0
                 for i in range(pl):
                     if word[i] == pref[i]:
                         count += 1
                 if count == pl:
-                    res += 1
-            
+                    res += 1 
         return res
