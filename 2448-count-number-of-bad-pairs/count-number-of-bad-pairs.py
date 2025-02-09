6e3a26c4afdb1b2aa4a5500f1897dd1031c21a2e
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-02-09 23:35:32 +0530

    Time: 77 ms (62.14%) | Memory: 38.9 MB (28.81%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -0,0 +1,25 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        c = 0
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if j-i != nums[j] - nums[i]:
+        #             c += 1
+        # return c
+
+        tot = n * (n - 1) // 2
+        freq = {}
+        for i, num in enumerate(nums):
+            diff = num - i
+            if diff in freq:
+                c += freq[diff]
+                freq[diff] += 1
+            else:
+                freq[diff] = 1
+
+        return tot - c
+        
+        
+        
+           
