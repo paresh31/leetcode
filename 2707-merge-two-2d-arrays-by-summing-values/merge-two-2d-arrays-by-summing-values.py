afb68f10029d50316e84dd00677e37817290f27b
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-03-03 01:35:19 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (17.98%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,11 +1,8 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        nums, r = nums1 + nums2, {}
-        for i, v in nums:
-            if i in r:
-                r[i] += v
-            else:
-                r[i] = v
+        nums, r = nums1 + nums2, defaultdict(int)
+        for k, v in nums:
+            r[k] += v
         ma = []
         for i, v in sorted(r.items()):
             ma.append([i, v])
