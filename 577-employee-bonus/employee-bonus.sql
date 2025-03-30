719a28d5a949c837dfa9b3b80c550f1fbbc36c30
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-03-31 00:23:40 +0530

    Time: 1083 ms (86.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select Employee.name, Bonus.bonus from Employee left join Bonus on Employee.empID = Bonus.empID where Bonus.bonus < 1000 or Bonus.bonus is null;
