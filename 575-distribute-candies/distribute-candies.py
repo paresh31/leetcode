2fce8d90f793415d549db03aa9805addb4fdec53
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-05-05 19:29:41 +0530

    Time: 51 ms (8.37%) | Memory: 18.9 MB (99.46%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,6 +1,14 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
         n = len(candyType)
-        s = len(set(candyType))
-        r = n // 2
-        return min(s, r)
+        # r = n // 2
+        # return min(s, r)
+        d = dict()
+        for item in candyType:
+            if item in d:
+                d[item] += 1
+            else:
+                d[item] = 1
+        d_len = len(d)
+        return min(n // 2, d_len)
+        
