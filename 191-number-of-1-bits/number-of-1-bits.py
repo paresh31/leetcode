f9f23720f57710fd28f57aa52273fec4eaf36234
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-05-06 19:17:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.69%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -6,8 +6,15 @@ class Solution:
         #     if item == "1":
         #         count += 1
         # return count
+        # count = 0
+        # while n:
+        #     count += n & 1
+        #     n = n >> 1
+        # return count
+
+
         count = 0
         while n:
-            count += n & 1
-            n = n >> 1
+            n = n & n - 1
+            count += 1
         return count
