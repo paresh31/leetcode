4b359e793409a3311746a80961bca39c629f445f
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-05-06 19:49:30 +0530

    Time: 30 ms (95.38%) | Memory: 17.8 MB (56.50%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -8,6 +8,7 @@ class Solution:
 
         res = 0
         for i in range(32):
-            res = res << 1 | (n & 1)
+            res = res << 1
+            res = res | n & 1
             n >>= 1
         return res
