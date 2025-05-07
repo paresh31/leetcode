f8313c2fe9eb7cc535c4fa357209f98fd8236456
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-05-07 20:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (95.76%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.py
+++ 371-sum-of-two-integers/sum-of-two-integers.py
@@ -1,3 +1,9 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return a + b
+        mx = 0xFFFFFFFF
+        mk = 0x7FFFFFFF       
+        while b != 0:
+            c = (a ^ b) & mx
+            b = (a & b) << 1
+            a = c & mx
+        return a if a <= mk else ~(a ^ mx)
