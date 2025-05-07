c0745a3f476cbb68e6bc8bd0b03bbaf486f8d47c
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2025-05-07 08:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (32.01%) - LeetSync

--- 137-single-number-ii/single-number-ii.py
+++ 137-single-number-ii/single-number-ii.py
@@ -1,11 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         d = dict()
-        for item in nums:
-            if item in d:
-                d[item] += 1
-            else:
-                d[item] = 1
+        for item in nums: d[item] = d.get(item, 0) + 1
         for k, v in d.items():
-            if d[k] == 1:
+            if d[k] != 3:
                 return k
