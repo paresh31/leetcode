1ad31ff5291d490812b3757253b069e0ae3612da
Paresh Kumar Mahato <22a91a6141@aec.edu.in>
2026-01-11 12:14:15 +0530

    Time: 1 ms (44.14%) | Memory: 19.9 MB (5.72%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -2,13 +2,8 @@ class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         m1 = m2 = m3 = float('-inf')
         for n in nums:
-            if n > m1:
-                m3 = m2
-                m2 = m1
-                m1 = n
-            elif n > m2 and n < m1:
-                m3 = m2
-                m2 = n
-            elif n > m3 and n < m2:
-                m3 = n
+            if n in (m1, m2, m3): continue
+            if n > m1: m3, m2, m1 = m2, m1, n
+            elif n > m2 and n < m1: m3, m2 = m2, n
+            elif n > m3 and n < m2: m3 = n
         return m3 if m3 != float('-inf') else m1
